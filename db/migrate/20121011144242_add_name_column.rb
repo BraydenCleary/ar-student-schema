97286f1c6220f167914122e68477e1fe70c8c4bd
Brayden Cleary <brayden.cleary@gmail.com>
2013-02-14 19:36:13 -0800

    wrote symmetric migration file for adding name column to students table and deleting first_name and last_name

---
+++ db/migrate/20121011144242_add_name_column.rb
@@ -0,0 +1,24 @@
+class AddNameColumn < ActiveRecord::Migration
+
+	def up
+		add_column :students, :name, :string
+		Students.all.each do |student|
+			student.name = student.first_name + " " + student.last_name
+			student.save
+		end
+		remove_column :students, :first_name
+		remove_column :students, :last_name
+	end
+
+	def down
+		add_column :students, :last_name, :string
+		add_column :students, :first_name, :string
+		Students.all.each do |student|
+			student.first_name = student.split ' '[0]
+			student.last_name  = student.split ' '[1]
+			student.save
+		end
+		remove_column :students, :name
+	end
+
+end
